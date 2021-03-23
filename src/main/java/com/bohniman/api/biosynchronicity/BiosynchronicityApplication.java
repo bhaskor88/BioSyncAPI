924a8d253522efed06afac473b9396d9d35de505
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2021-03-23 13:11:03 +0530

    First Commit

---
+++ src/main/java/com/bohniman/api/biosynchronicity/BiosynchronicityApplication.java
@@ -0,0 +1,13 @@
+package com.bohniman.api.biosynchronicity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiosynchronicityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiosynchronicityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bohniman/api/biosynchronicity/BiosynchronicityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohniman.api.biosynchronicity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiosynchronicityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
